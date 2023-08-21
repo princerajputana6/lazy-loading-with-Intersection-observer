005a595e0bef13db813cbd72ec5d39967ba9d829
Prince Rajput <prince.kumar@aurigait.com>
2023-08-21 18:29:30 +0530

    initial dev

---
+++ script.js
@@ -0,0 +1,36 @@
+const cards =  document.querySelectorAll(".card");
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry =>{
+      entry.target.classList.toggle("show", entry.isIntersecting)
+      if (entry.isIntersecting) observer.unobserve(entry.target)
+    })
+  },
+  {
+    threshold: 1,
+  }
+)
+
+const imgObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      const src = img.getAttribute('data-src');
+      img.setAttribute('src', src);
+      imgObserver.unobserve(img);
+    }
+  });
+}, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.1,
+});
+
+const lazyImages = document.querySelectorAll('img[data-src]');
+  lazyImages.forEach(img => {
+  imgObserver.observe(img);
+});
+
+cards.forEach(card =>{
+  observer.observe(card)
+})
